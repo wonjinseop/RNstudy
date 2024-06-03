8e17289ea534fdfbc979f2a20d635775f93ae77c
wonjinseop <wonjinseop7@naver.com>
2024-06-03 13:21:09 +0900

    #2. 할 일 입력 이벤트 처리, 할 일 목록 렌더링

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  // 입력창의 상태를 관리하는 변수를 React에서 사용하는 useState 훅을 사용하여 선언.
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [todoGoals, setTodoGoals] = useState([]);
+
+  // 사용자가 내용을 입력할 때 해당 입력값을 가져오는 함수
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  // 추가 버튼을 누르면 할 일 목록을 추가하는 함수
+  const addGoalHandler = () => {
+    // useState setter 메서드의 스냅샷 방식
+    // 콜백 함수의 매개값은 해당 상태 변수의 최신 값이 전달됨.
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
+      { text: enteredGoalText, key: Math.random().toString() },
+    ]);
+    // setTodoGoals([...todoGoals, enteredGoalText]);
+  };
+
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder='할 일을 입력하세요!' />
-        <Button title='할 일 추가하기' />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='할 일을 입력하세요!'
+          onChangeText={goalInputHandler}
+        />
+        <Button title='할 일 추가하기' onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text style={{ margin: 16 }}>할 일 목록들...</Text>
+      <View style={styles.goalsContainer}>
+        {todoGoals.map((goal) => (
+          <View style={styles.goalItem} key={goal.key}>
+            <Text style={styles.goalText}>{goal.text}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -16,6 +45,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
   },
 });
